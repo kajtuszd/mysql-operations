71a738f3242cc7356b6f78b67df5ef539805387f
kajetan <kajetanzdanowicz@wp.pl>
2021-05-26 17:58:49 +0200

    copying table to second database

--- code/main.py
+++ code/main.py
@@ -1,24 +1,55 @@
-import mysql.connector
 import os
+import mysql.connector
+
+
+def extract_create_table_query(cursor):
+    create_table_query = ""
+    show_create_table_query = """SHOW CREATE TABLE employees.titles;"""
+    cursor.execute(show_create_table_query)
+    create_table_info = cursor.fetchall()
+    for element in create_table_info[0][1:][0]:
+        create_table_query += element
+    return create_table_query
+
 
 try:
-    connection = mysql.connector.connect(host=os.environ['HOST_2'],
+    connection1 = mysql.connector.connect(host=os.environ['HOST_1'],
+                                         database=os.environ['DATABASE_1'],
+                                         user=os.environ['USER_1'],
+                                         password=os.environ['PASSWORD_1'])
+
+    connection2 = mysql.connector.connect(host=os.environ['HOST_2'],
                                          database=os.environ['DATABASE_2'],
                                          user=os.environ['USER_2'],
                                          password=os.environ['PASSWORD_2'])
-    cursor = connection.cursor()
-    show_table_query = """SHOW TABLES FROM employees_copy"""
-    cursor.execute(show_table_query)
-    result = cursor.fetchall()
-    print(result)
 
-    print("Table shown successfully ")
+    cursor1 = connection1.cursor()
+    cursor2 = connection2.cursor()
+
+    create_table_query = extract_create_table_query(cursor1)
+    cursor2.execute("""{}""".format(create_table_query))
+
+    show_table_query = """SELECT * FROM employees.titles"""
+    cursor1.execute(show_table_query)
+    records = cursor1.fetchall()
+    print("\n\n________________\n")
+
+    for record in records:
+        insert_to_table_query = """INSERT INTO employees_copy.titles(
+        emp_no, title, from_date, to_date) VALUES ('{}','{}','{}','{}');""".format(
+            record[0], record[1], record[2], record[3])
+        cursor2.execute(insert_to_table_query)
+    connection2.commit()
 
 except mysql.connector.Error as error:
-    print("Failed to show table: {}".format(error))
-finally:
-    if connection.is_connected():
-        cursor.close()
-        connection.close()
-        print("MySQL connection is closed")
+    print("Error: {}".format(error))
 
+finally:
+    if connection1.is_connected():
+        cursor1.close()
+        connection1.close()
+        print("MySQL connection1 is closed")
+    if connection2.is_connected():
+        cursor2.close()
+        connection2.close()
+        print("MySQL connection2 is closed")
